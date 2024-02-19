cd99993fea7d467821ff7755b8e12ef502ce7b48
MirHasham Rashid <mirhashem.rashid@gmail.com>
2024-02-19 09:51:48 +0430

    bugs solved

--- src/components/carousel-hero/HeroSection.js
+++ src/components/carousel-hero/HeroSection.js
@@ -3,39 +3,21 @@ import React from 'react';
 import {  SwiperSlide,Swiper } from 'swiper/react';
 import 'swiper/swiper-bundle.css';
 import './heroSection.css'
-import { Navigation,Autoplay,EffectFade,EffectCreative,EffectFlip } from 'swiper/modules';
-
-// SwiperCore.use([Navigation, Pagination, Autoplay]);
+import { Navigation,Autoplay} from 'swiper/modules';
 
 const HeroSection = ({ slides }) => {
-//   useEffect(() => {
-//     const swiper = new Swiper('.swiper-container', {
-//       loop: true,
-//       autoplay: {
-//         delay: 2000,
-//         disableOnInteraction: false
-//       },
-//       navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev',
-//       },
-//     });
-//   }, []);
    
   return (
     <div className="swiper-container">
       <Swiper
         loop={true}
         autoplay={{ delay: 5000 }}
-       
-        // pagination={{ clickable: true }}
-        // fadeEffect={EffectFlip}
         className='swiper'
         modules={[Navigation,Autoplay]}
       >
         {slides.map((slide, index) => (
           <SwiperSlide key={index} className='swiper-slide'>
-            <img className='slide-image' src={slide.image} alt={`Image ${index + 1}`} />
+            <img className='slide-image' src={slide.image} alt={`hero ${index + 1}`} />
             <div className="swiper-slide-caption">{slide.caption}</div>
           </SwiperSlide>
         ))}
